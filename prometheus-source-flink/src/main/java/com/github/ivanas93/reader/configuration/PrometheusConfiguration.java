44ca29ced3d9552821782d41a689c03cdcc41992
ivanas93 <ancarluji@gmail.com>
2022-01-11 16:50:27 +0100

    GH-14 as server params as config

--- prometheus-source-flink/src/main/java/com/github/ivanas93/reader/configuration/PrometheusConfiguration.java
+++ prometheus-source-flink/src/main/java/com/github/ivanas93/reader/configuration/PrometheusConfiguration.java
@@ -10,10 +10,14 @@ public class PrometheusConfiguration implements Serializable {
 
     private final String path;
     private final Integer port;
+    private final Long waitingInitialization;
+    private final Integer stopDelay;
 
     public PrometheusConfiguration(final String prefix, final Map<String, String> configuration) {
         path = configuration.getOrDefault(prefix + ".path", "/api/v1/write");
         port = Integer.valueOf(configuration.getOrDefault(prefix + ".port", "9090"));
+        waitingInitialization = Long.valueOf(configuration.getOrDefault(prefix + ".waiting-initialization", "5000"));
+        stopDelay = Integer.valueOf(configuration.getOrDefault(prefix + ".stop-delay", "3"));
     }
 
 }
